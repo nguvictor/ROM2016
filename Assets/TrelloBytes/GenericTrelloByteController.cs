bda53e2e32e148e2050c5e15775b098b16700ce8
tudortibu <tudortibu@gmail.com>
2016-09-16 17:09:06 -0400

    Adding Generic TrelloByteClass

---
+++ Assets/TrelloBytes/GenericTrelloByteController.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GenericTrelloByteController : MonoBehaviour {
+
+    enum trelloState {walking, falling, digging, climber, bashing };
+    bool isClimber;
+
+    trelloState currentState = trelloState.walking;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
