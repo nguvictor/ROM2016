26a4e9f8e2b9f6a4919d53d823de7c8d7ae7916c
Tyler Zanon <tdzanon@gmail.com>
2016-09-16 18:24:30 -0400

    blah

---
+++ Assets/Scripts/TriloController.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class TriloController : MonoBehaviour {
+
+    private enum states { WALK, DIG_DOWN, DIG_SIDEWAYS, CLIMB_UP};
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    
+	}
+
+    // For movement
+    void FixedUpdate()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+
+    }
+
+    public void Jump()
+    {
+
+    }
+
+    protected void Die()
+    {
+        Destroy(this);
+    }
+
+}
